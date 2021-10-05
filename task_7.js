04dc0f2031bc0e21cba1955039ae2d1747996827
kerevkerev <75122683+kerevkerev@users.noreply.github.com>
2021-10-05 21:26:03 +0700

    creat task_7

--- task_7.js
+++ task_7.js
@@ -1,12 +1,13 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,0,"dsf"];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// проверка на наличие не чисел
 if (arr.some(item => item === 0)) {
     console.log("В массиве есть нулевой элемент ")
 }
-else if (arr.some(item => isNaN(item))) {
+if (arr.some(item => typeof item == "string" || typeof item == "object"|| typeof item == "symbol"|| typeof item == "null")) {
     console.log("В массиве содержатся не только числа")
-
 }
-
+// проверка на наличие четных и нечетных чисел и их кол-во
 let even = arr.map(x => x % 2).filter(x => x == 0).length
 let odd = arr.map(x => x % 2).filter(x => x == 1).length
 console.log(`четных чисел ${even}`)
